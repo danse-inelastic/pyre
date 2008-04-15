7c781ec67cead66c8f1ea580f4524a1f48660e60
Michael McKerns <mmckerns@caltech.edu>
2008-04-15 18:41:45 +0000

    added build for python-only .zip file

--- Make.mm
+++ Make.mm
@@ -4,7 +4,7 @@
 #
 #                               Michael A.G. Aivazis
 #                        California Institute of Technology
-#                        (C) 1998-2005  All Rights Reserved
+#                        (C) 1998-2008  All Rights Reserved
 #
 # <LicenseText>
 #
@@ -18,6 +18,14 @@ RECURSE_DIRS = \
     tests \
     applications \
     examples \
+
+
+#To use the pure python versions of selected pythia packages,
+#you must add the produced .zip file to your PYTHONPATH.
+PYTHIA_VERSION = 0.8
+PYTHIA_ZIP = pythia-${PYTHIA_VERSION}.zip
+ZIP_PACKAGES = pyre journal opal
+
 
 #--------------------------------------------------------------------------
 #
@@ -34,6 +42,16 @@ distclean::
 
 tidy::
 	BLD_ACTION="tidy" $(MM) recurse
+
+zip:
+	(cd packages; \
+	for dir in ${ZIP_PACKAGES}; do { \
+             (cd $${dir}; zip -r ../../${PYTHIA_ZIP} $${dir} -i \*.py); \
+	} ; done )
+
+zip0: 
+	(cd packages/pyre; zip -r ../../${PYTHIA_ZIP} pyre -i \*.py; \
+        cd ../journal; zip -r ../../${PYTHIA_ZIP} journal -i \*.py)
 
 
 # version
