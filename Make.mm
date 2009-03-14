25700a7161465ba920e9d52fed73ff698341d474
Michael McKerns <mmckerns@caltech.edu>
2009-03-14 01:37:14 +0000

    mm zip3 now adds the tests directory to the distro

--- Make.mm
+++ Make.mm
@@ -52,12 +52,25 @@ zip2: prefix
 	(cd packages/pyre; zip -r ../../${PYTHIA_ZIP} pyre -i \*.py; \
         cd ../journal; zip -r ../../${PYTHIA_ZIP} journal -i \*.py)
 
-zip3: zip
+zip3: zip2
 	$(MV_F) ./${PYTHIA_ZIP} ./${PYTHIA_ZIP3}
 
+zip4: addtests zip3 deltests
+
+
+### slave calls ###
 PREFIX_DIR = ./packages/pyre/pyre/inventory/odb/
 prefix:
 	$(CP_F) ${PREFIX_DIR}/prefix-template.py ${PREFIX_DIR}/prefix.py
+
+CPFLAGS = -rf
+addtests:
+	$(CP) ${CPFLAGS} packages/pyre/tests packages/pyre/pyre/tests
+	$(CP) ${CPFLAGS} packages/journal/tests packages/journal/journal/tests
+
+deltests:
+	$(RM) $(RMFLAGS) packages/pyre/pyre/tests
+	$(RM) $(RMFLAGS) packages/journal/journal/tests
 
 
 # version
