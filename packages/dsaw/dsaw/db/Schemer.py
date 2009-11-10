9bf85e395077c6f581f11ccd6f23870a3ff30806
J Brandon Keith <jbrkeith@gmail.com>
2009-11-10 11:58:35 +0000

    trying a new philosophy in automatic db'ing an object: instead of trying to assign *everything* a column, we are just assigning columns to attributes of type int,bool,str,dict,list,tuple,float,Column,or a reference to another Table class...this has the added advantage i no longer have to explicitly ignore functions...backward compatibility is fully achieved if i keep 'name' as a reserved word (otherwise name is take from type(self).__name__.lower() )...

--- packages/dsaw/dsaw/db/Schemer.py
+++ packages/dsaw/dsaw/db/Schemer.py
@@ -26,10 +26,15 @@ class Schemer(base):
     """scan the class record for implied attributes 
     and put those in columnRegistry.
     
+    check if the types are
+    actual instances of int,str,float,list,tuple,dict, a subclasses of Column, or a 
+    reference to an object that is a subclass of Table
+    and then attempt to store them if their name matches up   
+    
     todo: add possibility of getting table name from the class name.lower()
     """
 
-    def __init__(cls, name, bases, dict):
+    def __init__(cls, name, bases, dict):    
         # the base class will get all the publicly declared columns and make them
         # part of the registry
         base.__init__(cls, name, bases, dict)
@@ -37,21 +42,24 @@ class Schemer(base):
         # of classes should be combined
         
         #now we get the 'implied columns' (those without an explicit declaration)
-        #impliedTypes = {}
         for name, item in cls.__dict__.iteritems():
-
+            
+#            if name in ('Schemer','_columnRegistry','_writeable','name','dsaw','VersatileReference'):
+#                continue
+            if name in ('_columnRegistry','_writeable','name'):
+                continue
             # disregard entries that are already in the column registry
             if name in cls._columnRegistry.keys():
                 continue
             # disregard metadata
             if name[:2]=='__' and name[-2:]=='__':
                 continue
-            # disregard functions
-            if type(item).__name__=='function':
-                continue
+#            # disregard functions
+#            if type(item).__name__=='function':
+#                continue
 
             # register it and apply automatic type detector
-            cls._columnRegistry[item.name] = assignType(item)
+            assignType(cls, item)
             
             if not cls._columnRegistry[item.name].auto:
                 cls.writeable.append(item.name)
@@ -70,13 +78,21 @@ def assignType(cls, item):
     import dsaw.db
     #attributeType = type(item).__name__ or for class do type(self).__name__
     if isinstance(item,type(1)):
-        return dsaw.db.integer(name=item.name, default=item)
+        cls._columnRegistry[item.name] = dsaw.db.integer(name=item.name, default=item)
     elif isinstance(item,type(1.0)):
-        return dsaw.db.double(name=item.name, default=item)
+        cls._columnRegistry[item.name] = dsaw.db.double(name=item.name, default=item)
     elif isinstance(item,type([])) or isinstance(item,type((1,))):
-        return dsaw.db.varcharArray(name=item.name, length=64, default=item)
-    else:
-        return dsaw.db.varchar(name=item.name, length=64, default=item)
+        cls._columnRegistry[item.name] = dsaw.db.varcharArray(name=item.name, length=64, default=item)
+    elif isinstance(item,type({})):
+        cls._columnRegistry[item.name+'_keys'] = dsaw.db.varcharArray(name=item.name+'_keys', length=64, default=item.keys())
+        cls._columnRegistry[item.name+'_values'] = dsaw.db.varcharArray(name=item.name+'_values', length=64, default=item.values())
+    elif isinstance(item,type('abc')):
+        cls._columnRegistry[item.name] = dsaw.db.varchar(name=item.name, length=64, default=item)
+    elif isinstance(item, Column):
+        #this is taken care of for now by the parent Schemer
+        pass
+    elif isinstance(item, Table):
+        cls._columnRegistry[item.name] = dsaw.db.reference()
 
             
             
