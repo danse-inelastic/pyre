84f3fd4bf453773736c31a4a3aac1a2a41cca6fe
J Brandon Keith <jbrkeith@gmail.com>
2010-01-30 01:38:13 +0000

    fixing pyre Make so it can automatically create a zip file with dsaw in it (Jiao take note)...now alex will create a cron job that refreshes the pyre download zip file with this latest build each night

--- Make.mm
+++ Make.mm
@@ -15,6 +15,7 @@ PACKAGE = pyre
 
 PYTHIA_VERSION = 0.8
 PYTHIA_ZIP = pythia-${PYTHIA_VERSION}.zip
+PYTHIA_ZIP3 = pythia-${PYTHIA_VERSION}-trunk.zip
 
 RECURSE_DIRS = \
     packages \
@@ -55,13 +56,42 @@ zip2: prefix
 	(cd packages/pyre; \
         zip -r ../../${PYTHIA_ZIP} pyre -i \*.py ${PYRE_EXTENSIONS}; \
         cd ../journal; \
-        zip -r ../../${PYTHIA_ZIP} journal -i \*.py ${PYRE_EXTENSIONS})
+        zip -r ../../${PYTHIA_ZIP} journal -i \*.py ${PYRE_EXTENSIONS}; \
+		cd ../dsaw; \
+		echo `pwd`; \
+        zip -r ../../${PYTHIA_ZIP} dsaw -i \*.py ${PYRE_EXTENSIONS})
 
+zip3: zip2
+	(cd packages/pyre; zip -r ../../${PYTHIA_ZIP} setup.py)
+	$(MV_F) ./${PYTHIA_ZIP} ./${PYTHIA_ZIP3}
+
+zip4: addtests etczip zip3 deltests
 
 ### slave calls ###
 PREFIX_DIR = ./packages/pyre/pyre/inventory/odb/
 prefix:
 	$(CP_F) ${PREFIX_DIR}/prefix-template.py ${PREFIX_DIR}/prefix.py
+
+CPFLAGS = -rf
+addtests:
+	$(CP) ${CPFLAGS} packages/pyre/tests packages/pyre/pyre/tests
+	$(CP) ${CPFLAGS} packages/journal/tests packages/journal/journal/tests
+
+deltests:
+	$(RM) $(RMFLAGS) packages/pyre/pyre/tests
+	$(RM) $(RMFLAGS) packages/journal/journal/tests
+
+ODB_ZIP = pythia-odb.zip
+etczip:
+	(cd packages/pyre/etc; \
+        zip -r ../../../${ODB_ZIP} idd ipa weaver -i ${PYRE_EXTENSIONS})
+	$(MKDIR) $(MKPARENTS) packages/journal/xxetcxx
+	$(CP) ${CPFLAGS} packages/journal/etc packages/journal/xxetcxx/journal
+	(cd packages/journal/xxetcxx; \
+        zip -r ../../../${ODB_ZIP} journal -i ${PYRE_EXTENSIONS})
+	$(RM) $(RMFLAGS) packages/journal/xxetcxx
+	(zip -r ${PYTHIA_ZIP} ${ODB_ZIP})
+	$(RM) $(RMFLAGS) ${ODB_ZIP}
 
 
 # version
